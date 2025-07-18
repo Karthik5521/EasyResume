4ba0c0fcc51ca03717feb562d9fa6e6a4e7b5bb0
Karthik5521 <majjigakarthik5@gmail.com>
2025-07-17 22:19:22 -0500

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react' // ✅ Import Analytics
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
-  </>,
+    <Analytics /> 
+  </>
 )
